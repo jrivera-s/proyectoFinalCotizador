cf3e468f1c5ffc41ebea4148e7ba46d5d8a8f879
rivera <rivera@riveras-MacBook-Air.local>
2023-02-20 22:31:57 -0600

    ajustes en alertas de recargos

--- cotizador.js
+++ cotizador.js
@@ -5,11 +5,14 @@ const recargo1 = 0.10; // 10%
 const recargo2 = 0.20; // 20%
 const recargo3 = 0.30; // 30%
 
+let nombreAsegurado = prompt("Ingrese nombre del asegurado: ")
+
 // Definimos las variables de entrada
-let edad = parseInt(prompt("Ingrese la edad del asegurado:"));
+let edad = parseInt(prompt("Ingrese la edad del asegurado: "));
 
 // precio base
 let precioBase = 2000;
+
 
 // Calculamos el recargo por edad del asegurado
 let recargoAsegurado = 0;
@@ -26,7 +29,7 @@ let edadConyuge = 0;
 
 // Si el asegurado está casado, pedimos la edad de su cónyuge
 if (estaCasado) {
-  edadConyuge = parseInt(prompt("Ingrese la edad de su cónyuge:"));
+  edadConyuge = parseInt(prompt("Ingrese la edad de su cónyuge: "));
 }
 
 // Calculamos el recargo por edad del cónyuge, si aplica
@@ -46,18 +49,19 @@ let tieneHijos = prompt("¿Tiene hijos? (S/N)").toLowerCase() === 's';
 let cantidadHijos;
 let recargoHijos = 0;
 if (tieneHijos==true) {
-  let cantidadHijos = parseInt(prompt("Ingrese la cantidad de hijos:"));
+  let cantidadHijos = parseInt(prompt("Ingrese la cantidad de hijos: "));
    recargoHijos = cantidadHijos * recargo2;
 }
+let totalRecargos = recargoAsegurado + recargoConyuge + recargoHijos;
 
 //salida del codigo
-console.log("total recargo Asegurado: " + (100 *recargoAsegurado.toFixed(2)) + "%");
-console.log("total recargo Conyugue: " + (100 * recargoConyuge.toFixed(2))+ "%");
-console.log("total recargo hijos: " + (100 * recargoHijos.toFixed(2)) + "%");
+alert("total recargo Asegurado: " + (100 *recargoAsegurado.toFixed(2)) + "%\n", "total recargo Conyugue: " + (100 * recargoConyuge.toFixed(2))+ "%\n", "total recargo hijos: " + (100 * recargoHijos.toFixed(2)) + "%\n", "Recargo Total: " + (100 * totalRecargos.toFixed(2)) + "%");
+//alert("total recargo Conyugue: " + (100 * recargoConyuge.toFixed(2))+ "%");
+//alert("total recargo hijos: " + (100 * recargoHijos.toFixed(2)) + "%");
 
-let totalRecargos = recargoAsegurado + recargoConyuge + recargoHijos;
-console.log("Recargo Total: " + (100 * totalRecargos.toFixed(2)) + "%");
+
+//alert("Recargo Total: " + (100 * totalRecargos.toFixed(2)) + "%");
 
 let totalCotizacion = precioBase + (precioBase * totalRecargos);
 
-console.log("total cotización: " + "Q." + totalCotizacion.toFixed(2) );
+alert("Para el asegurado: " + nombreAsegurado + " la cotización es: " + "Q." + totalCotizacion.toFixed(2) );
